51db6e986f6160738a05383524c528e8fc4ebeb7
sergey.binder <fornewreal@gmail.com>
2021-09-18 10:33:44 +0300

    Changed DeathZone mechanic

--- Assets/Scripts/Gameplay/DeathZone.cs
+++ Assets/Scripts/Gameplay/DeathZone.cs
@@ -1,26 +1,15 @@
 using UnityEngine;
 
 using System.Collections.Generic;
-using System.Linq;
 
 using SF.Common;
 using SF.Managers;
 
 namespace SF.Gameplay {
 	public sealed class DeathZone : GameComponent {
-		sealed class BoxTimer {
-			public readonly BaseBox Box;
-			public          float   Timer;
+		public float Dps = 100f;
 
-			public BoxTimer(BaseBox box) {
-				Box   = box;
-				Timer = 0f;
-			}
-		}
-
-		public float DeathTime = 3f;
-
-		readonly List<BoxTimer> _boxTimers = new List<BoxTimer>();
+		readonly List<BaseBox> _boxes = new List<BaseBox>();
 
 		PauseManager _pauseManager;
 
@@ -32,22 +21,21 @@ namespace SF.Gameplay {
 			if ( _pauseManager.IsPaused ) {
 				return;
 			}
-			for ( var i = _boxTimers.Count - 1; i >= 0; i-- ) {
-				var boxTimer = _boxTimers[i];
-				boxTimer.Timer += Time.deltaTime;
-				if ( boxTimer.Timer >= DeathTime ) {
-					_boxTimers.RemoveAt(i);
-					boxTimer.Box.Die();
+			for ( var i = _boxes.Count - 1; i >= 0; i-- ) {
+				var box = _boxes[i];
+				box.TakeDamage(Dps * Time.deltaTime);
+				if ( !box ) {
+					_boxes.RemoveAt(i);
 				}
 			}
 		}
 
 		void OnTriggerEnter2D(Collider2D other) {
 			var box = other.GetComponentInChildren<BaseBox>();
-			if ( !box || _boxTimers.Any(x => x.Box == box) ) {
+			if ( !box || _boxes.Contains(box) ) {
 				return;
 			}
-			_boxTimers.Add(new BoxTimer(box));
+			_boxes.Add(box);
 		}
 
 		void OnTriggerExit2D(Collider2D other) {
@@ -55,12 +43,7 @@ namespace SF.Gameplay {
 			if ( !box ) {
 				return;
 			}
-			for ( var i = _boxTimers.Count - 1; i >= 0; --i ) {
-				var boxTimer = _boxTimers[i];
-				if ( boxTimer.Box == box ) {
-					_boxTimers.RemoveAt(i);
-				}
-			}
+			_boxes.Remove(box);
 		}
 	}
 }
